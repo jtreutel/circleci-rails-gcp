224d78bd6c54518ac39a56ccbfbbc6c554cb5807
jtreutel <jennings@circleci.com>
2021-04-21 10:57:22 +0900

    terraform fmt.

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -1,6 +1,6 @@
 ## Create Google Compute Instance and run bash script to install application
 resource "google_compute_instance" "rails_demo" {
-  name         = "circleci-rails-demo-${var.commit_hash != "" ? var.commit_hash :random_id.dns_name.hex}"
+  name         = "circleci-rails-demo-${var.commit_hash != "" ? var.commit_hash : random_id.dns_name.hex}"
   machine_type = var.machine_type
   zone         = "${var.gcp_region}-a"
 
@@ -42,7 +42,7 @@ resource "google_compute_firewall" "http-server" {
 ## Create record set in Google DNS Managed Zone
 resource "google_dns_record_set" "resource-recordset" {
   managed_zone = data.google_dns_managed_zone.production.name
-  name         = "railsdemo-${var.commit_hash != "" ? var.commit_hash :random_id.dns_name.hex}.ccigcp.jtreutel.io."
+  name         = "railsdemo-${var.commit_hash != "" ? var.commit_hash : random_id.dns_name.hex}.ccigcp.jtreutel.io."
   type         = "A"
   rrdatas      = [google_compute_instance.rails_demo.network_interface.0.access_config.0.nat_ip]
   ttl          = 120
